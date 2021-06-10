f53a7d8c3e277057848d3118152553afcfa5b58d
Pavan Kunchala <pavankunchalapk@gmail.com>
2021-06-11 01:09:47 +0530

    Add files via upload

--- face_mesh_app.py
+++ face_mesh_app.py
@@ -71,116 +71,6 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # return the resized image
     return resized
 
-st.subheader('We are applying Face Mesh on a video')
-    
-st.sidebar.text('Params For video')
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 400px;
-        margin-left: -400px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-        )
-detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
-racking_confidence = st.sidebar.slider('Min Tracking Confidence', min_value = 0.0,max_value = 1.0,value = 0.5)
-
-max_faces = st.sidebar.number_input('Maximum Number of Faces',value =1,min_value= 1)
-
-st.markdown(' ## Output')
-stframe = st.empty()
-    
-
-video_file_buffer = st.sidebar.file_uploader("Upload a video", type=[ "mp4", "mov",'avi','asf', 'm4v' ])
-
-    
-tfflie = tempfile.NamedTemporaryFile(delete=False)
-
-if not video_file_buffer:
-    
-    vid = cv2.VideoCapture(DEMO_VIDEO)
-    tfflie.name = DEMO_VIDEO
-    
-else:
-    tfflie.write(video_file_buffer.read())
-
-vid = cv2.VideoCapture(tfflie.name)
-width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = int(vid.get(cv2.CAP_PROP_FPS))
-    #codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
-    #codec = cv2.VideoWriter_fourcc('V','P','0','9')
-    #out = cv2.VideoWriter('output.webm', codec, fps, (width, height))
-
-
-st.sidebar.text('Input Video')
-st.sidebar.video(tfflie.name)
-
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-    
-
-with mp_face_mesh.FaceMesh(
-min_detection_confidence=detection_confidence,
-min_tracking_confidence=tracking_confidence , 
-max_num_faces = max_faces) as face_mesh:
-    
-
-
-
-    while vid.isOpened():
-        
-        ret, frame = vid.read()
-
-        if ret:
-            
-            pass
-                
-                
-            
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-            
-            
-
-        results = face_mesh.process(frame)
-
-        frame.flags.writeable = True
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-        if results.multi_face_landmarks:
-            
-            for face_landmarks in results.multi_face_landmarks:
-                    mp_drawing.draw_landmarks(
-                    image = frame,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACE_CONNECTIONS,
-                    landmark_drawing_spec=drawing_spec,
-                    connection_drawing_spec=drawing_spec)
-
-
-            #out.write(frame)    
-            #frame = cv2.resize(frame,(0,0),fx = 0.8 , fy = 0.8)
-
-        frame = image_resize(image = frame, width = 640)
-            
-        stframe.image(frame,channels = 'BGR',use_column_width=True)
-
-            
-    
-
-
-
-                
-vid.release()
-    #out.release()
-
 
 
 
@@ -299,10 +189,12 @@ elif app_mode =='Run on Video':
             ret, frame = vid.read()
 
             if ret:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                pass
                 
-            else:
-                print('Video not found')
+                
+            
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             
             
