5da49b621afd0978c0a60d28dbb8c8e9bb0fc196
Pavan Kunchala <pavankunchalapk@gmail.com>
2021-06-11 12:10:58 +0530

    Add files via upload

--- app_v2.py
+++ app_v2.py
@@ -139,7 +139,7 @@ def main():
 
             frame.flags.writeable = True
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            #maxframe = cv2.resize(frame,(0,0),fx = 0.5,fy = 0.5,interpolation = cv2.INTER_AREA)
+            
 
             if results.multi_face_landmarks:
                 for face_landmarks in results.multi_face_landmarks:
